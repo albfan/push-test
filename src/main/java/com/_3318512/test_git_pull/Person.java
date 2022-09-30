b9f6e4fd5c93829c9e544a55bcc63a62f2d781bb
rhpamAdmin <>
2022-09-30 11:29:08 +0200

    {/src/main/java/com/_3318512/test_git_pull/Person.java}

--- src/main/java/com/_3318512/test_git_pull/Person.java
+++ src/main/java/com/_3318512/test_git_pull/Person.java
@@ -6,12 +6,33 @@ package com._3318512.test_git_pull;
 
 public class Person implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Person() {
-    }
+	private java.lang.String name;
+	private java.lang.Integer age;
 
+	public Person() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.Integer getAge() {
+		return this.age;
+	}
+
+	public void setAge(java.lang.Integer age) {
+		this.age = age;
+	}
+
+	public Person(java.lang.String name, java.lang.Integer age) {
+		this.name = name;
+		this.age = age;
+	}
 
 }
